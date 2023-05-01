f44a6e04196d28379c341f362dbfdf4f3958a882
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2023-05-01 18:58:13 +0000

    404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
 """ api setup for AirBnB """
-from flask import Flask
+from flask import Flask, abort, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
-
-
-@app.teardown_appcontext
-def close(exception=None):
-    storage.close()
+app.url_map.strict_slashes = False
 
 
 @app.errorhandler(404)
-def page_not_found(error):
+def handle_404_error(e):
+    response = jsonify({'error': 'Not found'})
+    response.status_code = 404
+    return response
+
+@app.teardown_appcontext
+def close(exception=None):
     """
-    Function that shows a 404 error
+     Function closes the current session
     """
-    return (jsonify({error: "Not found"}), 404)
+    storage.close()
+
 
 
 if __name__ == "__main__":
