2cc53f11a03d5dfddb7c13e8d39c4a4bf8d90542
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2023-05-01 16:17:34 +0000

    count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,5 +9,19 @@ from models import storage
 
 
 @app_views.route('/status')
-def getStatus():
+def get_status():
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats')
+def get_count():
+    count_dict = {"amenities": 'Amenity',
+                  "cities": 'City',
+                  "places": 'Place',
+                  "reviews": 'Review',
+                  "states": 'State',
+                  "users": 'User'}
+
+    for key in count_dict.keys():
+        count_dict[key] = storage.count(count_dict.get(key))
+    return jsonify(count_dict)
